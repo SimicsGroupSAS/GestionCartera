4e31fc8adce8ee45da8c8e13af9f63d05f50a5c0
JeisonCerpa <cerpajeison8@gmail.com>
2025-04-01 08:01:00 -0500

    Add .gitignore, update CredentialDialog to allow editable BCC, and improve email validation

--- obj/Debug/net9.0-windows/MiProyectoWPF.AssemblyInfo.cs
+++ obj/Debug/net9.0-windows/MiProyectoWPF.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiProyectoWPF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa768cd7c36dd88ff4d38fdb3c64e56e9d97bfd1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a213f3d03e9ebe2164dc64d598617165c7b8e3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiProyectoWPF")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiProyectoWPF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
